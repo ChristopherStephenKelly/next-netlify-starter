2ca98329d4343ec71e2b834de4e0c7f0a581e939
ChristopherStephenKelly <93892962+ChristopherStephenKelly@users.noreply.github.com>
2022-10-24 21:48:45 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -11,11 +12,17 @@ export default function Home() {
       </Head>
 
       <main>
+    
         <Header title="Welcome to my app!" />
+         <h1 className="title">
+           Read <Link href="/posts/first-post">this page!</Link>
+      </h1>
+      </main>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-      </main>
+    
+    
 
       <Footer />
     </div>
